cffaf09f77886885b286fc54ab11546bac534e00
iuri-pdista <correaiuri86@gmail.com>
2020-08-12 11:23:26 -0300

    Updated remover.sh to match all current features

--- remover.sh
+++ remover.sh
@@ -5,22 +5,33 @@ if [ $1 = "-git" ]; then
 	do
 		echo "Will add to gitgnore"
 		echo "*$file_types" >> ./.gitignore
-	done	
+	done
+	echo "Starting clean with .gitignore..."	
+elif [ $1 = "-jrm" ]; then
+	echo "Starting clean..."
+	
+else
+	echo "Invalid option ;-;"
 fi  
 if [ -d "../rmvdFiles" ]; then
-       	echo "the dir already exists"
+		echo "the dir already exists"
 else
 	mkdir ../rmvdFiles
 	echo "Dir was succesfully created"
 fi
 for i  in "$@"
 do
-	filePath=$(pwd)
-	filePath="${filePath}/*$i" 
-	for f in $filePath
-	do 
-	  mv $f ../rmvdFiles  
-	done  
+	if [[ ($i == "-git") || ($i == "-jrm") ]]; then
+		continue
+	else
+		filePath=$(pwd)
+		filePath="${filePath}/*$i" 
+		for f in $filePath
+		do 
+		mv $f ../rmvdFiles  
+		done
+	fi       	
 done
+
 #SHELL
 
