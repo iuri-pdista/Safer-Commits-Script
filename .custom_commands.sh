a07a20768b74a1721ae6e2c9167a74ad3ed8affd
iuri-pdista <correaiuri86@gmail.com>
2020-08-13 01:48:54 -0300

    Add dir removal feature

--- .custom_commands.sh
+++ .custom_commands.sh
@@ -1,37 +1,53 @@
 #!/bin/bash
 function removeFiles() {
-	if [ $1 = "-git" ]; then
-		for file_types in "$@"
-		do
-			echo "Will add to gitgnore"
-			echo "*$file_types" >> ./.gitignore
-		done
-		echo "Starting clean with .gitignore..."	
-	elif [ $1 = "-jrm" ]; then
-		echo "Starting clean..."
+	if [ $2 = "-d " ]; then
+		if [ $1 = "-git" ]; then
+			for file_types in "$@"
+			do
+				echo "Will add to gitgnore"
+				echo "*$file_types" >> ./.gitignore
+			done
+			echo "Starting clean with .gitignore..."	
+		elif [ $1 = "-jrm" ]; then
+			echo "Starting clean..."
+		else
+			echo "Invalid option ;-;"
+		fi  
 		
 	else
-		echo "Invalid option ;-;"
-	fi  
-	if [ -d "../rmvdFiles" ]; then
-			echo "the dir already exists"
-	else
-		mkdir ../rmvdFiles
-		echo "Dir was succesfully created"
-	fi
-	for i  in "$@"
-	do
-		if [[ ($i == "-git") || ($i == "-jrm") ]]; then
-			continue
-		else
-			filePath=$(pwd)
-			filePath="${filePath}/*$i" 
-			for f in $filePath
-			do 
-			mv $f ../rmvdFiles  
+		if [ $1 = "-git" ]; then
+			for file_types in "$@"
+			do
+				echo "Will add to gitgnore"
+				echo "*$file_types" >> ./.gitignore
 			done
-		fi       	
-	done
+			echo "Starting clean with .gitignore..."	
+		elif [ $1 = "-jrm" ]; then
+			echo "Starting clean..."
+		
+		else
+			echo "Invalid option ;-;"
+		fi  
+		if [ -d "../rmvdFiles" ]; then
+				echo "the dir already exists"
+		else
+			mkdir ../rmvdFiles
+			echo "Dir was succesfully created"
+		fi
+		for i  in "$@"
+		do
+			if [[ ($i == "-git") || ($i == "-jrm") ]]; then
+				continue
+			else
+				filePath=$(pwd)
+				filePath="${filePath}/*$i" 
+				for f in $filePath
+				do 
+				mv $f ../rmvdFiles  
+				done
+			fi       	
+		done
+	fi
 }
 
 function resetFiles(){
